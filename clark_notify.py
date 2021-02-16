abe26cb79503d30cdd6b71d72d7fca10f77332a6
fazildgr8 <mohamedfazilsulaiman@gmail.com>
2021-02-16 00:24:08 -0500

    Updated

---
+++ clark_notify.py
@@ -0,0 +1,14 @@
+from ub_gym_notifier import*
+
+loc = 'Clark Hall'
+url_pre = 'https://www.signupgenius.com/go/8050848a9a729a0fe3'
+
+days = ['monday','tuesday','wednesday','thursday','friday']
+weekdays = [12,12,11,12,13]
+notify_endpoint = 'https://notify.run/KQQrnm4IqCuxC6D7'
+url = ''
+for i in range(len(days)):
+    url = url_pre + '-'+ days[i]+str(weekdays[i])
+    event_notifier(url,notify_endpoint,days[i])
+
+
