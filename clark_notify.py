504089a630fd29c8e854730aa70f4e16fcc5f2e8
fazildgr8 <mohamedfazilsulaiman@gmail.com>
2021-02-17 09:14:47 -0500

    Updated remove past days of the week

--- clark_notify.py
+++ clark_notify.py
@@ -1,4 +1,8 @@
 from ub_gym_notifier import*
+from datetime import date
+
+today = date.today()
+weekday_int = today.weekday()
 
 loc = 'Clark Hall'
 url_pre = 'https://www.signupgenius.com/go/8050848a9a729a0fe3'
@@ -9,9 +13,25 @@ notify_endpoint = 'https://notify.run/KQQrnm4IqCuxC6D7'
 url = ''
 
 def clark_notify_all():
-    for i in range(len(days)):
-        url = url_pre + '-'+ days[i]+str(weekdays[i])
-        event_notifier(url,notify_endpoint,days[i])
+    if(weekday_int==1):
+        days = days[1:]
+        days = days[1:]
+    if(weekday_int==2):
+        days = days[2:]
+        weekdays = weekdays[2:]
+    if(weekday_int==3):
+        days = days[3:]
+        weekdays = weekdays[3:]
+    if(weekday_int==4):
+        days = days[4:]
+        weekdays = weekdays[4:]
+    if(weekday_int==5):
+        days = days[5:]
+        weekdays = weekdays[5:]
+    if(len(days)>0):
+        for i in range(len(days)):
+            url = url_pre + '-'+ days[i]+str(weekdays[i])
+            event_notifier(url,notify_endpoint,days[i])
 
 
 
